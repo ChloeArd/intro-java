a7da75ef36f4ea6bd068d9eacfdcd8dd40b507dc
ChloeArd <chloe.ardoise@gmail.com>
2022-04-21 11:46:18 +0200

    introduction node.js

--- backend/expressApp.js
+++ backend/expressApp.js
@@ -12,10 +12,41 @@ expressApp.use((req, res, next) => {
     next();
 });
 
-expressApp.use((req, res, next) => {
+/*expressApp.use((req, res, next) => {
     res.send('Hello World !');
     next();
+});*/
+
+expressApp.get("/", (req, res, next) => {
+    const response = {
+      message: "Hello Wooooooorld",
+    }
+    res.json(response);
+    next();
 });
+
+expressApp.get("/hello", (req, res, next) => {
+    res.json({coucou: "Coucou !"});
+    next();
+});
+
+expressApp.get("/hello/:name/:age?", (req, res, next) => {
+    const params = req.params;
+    const message = undefined === params.age ? `Coucou ${req.params.name}` : `Coucou ${req.params.name}, vous avez ${params.age}`;
+    res.json({coucou: message });
+    next();
+});
+
+// Handle 404 errors
+expressApp.use(function (req, res, next) {
+    if (!req.route) {
+        res
+            .status(404)
+            .end()
+        ;
+        next();
+    }
+})
 
 expressApp.use((req, res) => {
     console.log("Code response: " + res.statusCode);
